94ae42a188e33e796509654dc6bed4e8b4db2255
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-05-16 19:26:48 +0530

    project with tailwind and flowbite installed

---
+++ commerceclass/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css';
+import Header from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <div>
+     
+      <Header />
+ 
+     
+    </div>
+  );
+}
+
+export default App;
